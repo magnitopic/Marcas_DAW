7a557d2d0767b0456898a0e3078ec0166671347e
magnitopic <magnitopic@gmail.com>
2023-05-12 12:12:50 +0200

    finished practica33

--- practicas_JS/practica33/script.js
+++ practicas_JS/practica33/script.js
@@ -1,9 +1,35 @@
 const submitButton = document.querySelector("#register");
-const name = document.querySelector("#name").value;
-const email = document.querySelector("#email").value;
-const terms = document.querySelector("input[name='genero']:checked").value;
+const errorBox = document.querySelector("#error-box");
 
-submitButton.addEventListener("click", (e)=>{
+submitButton.addEventListener("click", (e) => {
 	e.preventDefault();
-	console.log(terms);
-})
+
+	let flag = false;
+	let structure = "";
+	const name = document.querySelector("#name").value;
+	const email = document.querySelector("#email").value;
+	const genero = document.querySelector("input[name='genero']:checked");
+	const terms = document.querySelector("#terms").checked;
+
+	if (name == "") {
+		structure += "<li>Por favor completa el nombre</li>";
+		flag = true;
+	}
+	if (email == "") {
+		structure += "<li>Por favor completa el email</li>\n";
+		flag = true;
+	}
+	if (genero == null) {
+		structure += "<li>Por favor selecciona el sexo</li>\n";
+		flag = true;
+	}
+	if (terms == false) {
+		structure += "<li>Por favor acepta los términos y condiciones</li>\n";
+		flag = true;
+	}
+
+	if (flag) errorBox.style.display = "flex";
+	else errorBox.style.display = "none";
+
+	errorBox.innerHTML = structure;
+});
